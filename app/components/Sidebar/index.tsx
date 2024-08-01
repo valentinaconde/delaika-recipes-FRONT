ffb65197aa0bdd1648d7151730d47f11b51eb53b
valentinaconde <valencondde@gmail.com>
2024-08-01 13:16:11 -0300

    chore: Add Sidebar component to home layout

---
+++ app/components/Sidebar/index.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const drawerWidth = 240;
+
+export default function Sidebar() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Typography variant="h6" noWrap component="div">
+            CATEGORIAS
+        </Typography>
+        <List>
+          {['Recetas rapidas', 'Desayuno', 'Pastas', 'Guarniciones'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+
+      </Drawer>
+
+    </Box>
+  );
+}
