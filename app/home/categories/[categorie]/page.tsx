ab874f3e9cc36652169734f2d99aae57bcfe24a8
valentinaconde <valencondde@gmail.com>
2024-08-09 14:59:39 -0300

    feat: Add back button functionality to category page

--- app/home/categories/[categorie]/page.tsx
+++ app/home/categories/[categorie]/page.tsx
@@ -1,9 +1,22 @@
 
+'use client'
+import { useRouter } from 'next/navigation';
 import React from 'react';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 export default function Page() {
+
+  const router = useRouter();
+  const handleGoBack = () => {
+    router.back();
+  }
+
     return (
       <>
+        <div className='flex' onClick={handleGoBack}>
+        <KeyboardArrowLeftIcon />
+        <button >Volver</button>
+        </div>
         <p>CATEGORIA</p>
       </>
     )
