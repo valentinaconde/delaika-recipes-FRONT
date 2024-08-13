abf85a15e6c5adfb88cb982fc5166fad0104e82d
valentinaconde <valencondde@gmail.com>
2024-08-13 16:46:56 -0300

    refactor: Update Recipe page to fetch and display recipe details

--- app/home/categories/[category]/[recipe]/page.tsx
+++ app/home/categories/[category]/[recipe]/page.tsx
@@ -1,11 +1,54 @@
 
-import React from 'react';
+'use client'
+import { Recipes } from '@/app/interfaces/recipes';
+import { getRecipeById } from '@/app/services/dataMock.service';
+import { usePathname } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 export default function Recipe() {
-    return (
-      <>
-      <h2>Receta</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique necessitatibus consectetur vitae omnis, perspiciatis soluta dolorum fuga placeat. Debitis eligendi laborum nam quos ut temporibus impedit, laudantium odit deleniti numquam.</p>
-      </>
-    )
+
+  const path = usePathname()
+
+  const [recipeId, setRecipeId] = useState(0)
+  const [recipe, setRecipe] = useState<Recipes>()
+
+  const handleSetRecipe = () => {
+    console.log(recipeId)
+    const recipe = getRecipeById(Number(recipeId));
+    if (recipe) setRecipe(recipe)
+
   }
+
+  const handleGetRecipeId = () => {
+    setRecipeId(Number(path.split('/')[4]))
+  }
+
+
+  useEffect(() => {
+    handleGetRecipeId();
+  }, [])
+
+  useEffect(() => {
+    handleSetRecipe()
+  }, [recipeId])
+
+
+  return (
+    <>
+      <h1 className='text-lg pb-3'>{(recipe?.name)?.toLocaleUpperCase()}</h1>
+      <div className='flex'>
+        <img src={recipe?.imageUrl} alt={recipe?.name} className='h-96' />
+        <div className='ps-5'>
+          <p>INGREDIENTES</p>
+          <ul>
+            {
+              recipe?.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient.quantity} {ingredient.name}</li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
