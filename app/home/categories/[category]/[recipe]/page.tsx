e9967947ef238340d82d333cdf1c7b99dda45091
valentinaconde <valencondde@gmail.com>
2024-08-13 14:04:44 -0300

    feat: Add Recipe component to category page

---
+++ app/home/categories/[category]/[recipe]/page.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+export default function Recipe() {
+    return (
+      <>
+      <h2>Receta</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique necessitatibus consectetur vitae omnis, perspiciatis soluta dolorum fuga placeat. Debitis eligendi laborum nam quos ut temporibus impedit, laudantium odit deleniti numquam.</p>
+      </>
+    )
+  }
