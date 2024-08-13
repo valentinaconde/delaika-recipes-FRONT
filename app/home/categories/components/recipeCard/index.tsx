7e5f76f3261e624916fc2fc3cfa805eac1011749
valentinaconde <valencondde@gmail.com>
2024-08-13 16:47:11 -0300

    refactor: Rename Recipe component to RecipeCard component

--- app/home/categories/components/recipeCard/index.tsx
+++ app/home/categories/components/recipeCard/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import style from './recipe.module.css';
 
-export default function Recipe({name= 'Nombre random', url="https://media.istockphoto.com/id/1337716828/es/foto/barbacoa-argentina.jpg?s=1024x1024&w=is&k=20&c=wQQavZxKDCNs-qQqX8KnsUP1pxaz2r9G_cZdHYO74kw="}: {name: string, url: string}) {
+export default function RecipeCard({name= 'Nombre random', url="https://media.istockphoto.com/id/1337716828/es/foto/barbacoa-argentina.jpg?s=1024x1024&w=is&k=20&c=wQQavZxKDCNs-qQqX8KnsUP1pxaz2r9G_cZdHYO74kw="}: {name: string, url: string}) {
     return (
     <div className={style.recipeCard}>
         <img src={url} alt={name}/>
