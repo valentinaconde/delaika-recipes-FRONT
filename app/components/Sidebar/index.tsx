65c5311af70b6d409ff7665f8a4f0544b26c02c0
valentinaconde <valencondde@gmail.com>
2024-08-13 14:39:31 -0300

    feat: Update Sidebar component to use dynamic class for active category link

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -9,11 +9,22 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import style from './sidebar.module.css';
 import { ROUTES } from '../../utils/routes';
+import {getCategories} from '../../services/dataMock.service';
+import {Category} from '../../interfaces/categories';
+import { useEffect } from 'react';
+
 export default function Sidebar() {
 
   const pathName = usePathname()
 
-  const [categories, setCategories] = useState(['Desayuno', 'Pastas', 'Guarniciones', 'Entradas'])
+  const [categories, setCategories] = useState<Category[]>()
+
+
+  useEffect(() => {
+    const categories = getCategories()
+    setCategories(categories)
+  }, [])
+  
   return (
     <div className='shadow-lg h-screen px-5 py-2'>
 
@@ -21,10 +32,10 @@ export default function Sidebar() {
         CATEGORIAS
       </Typography>
       <List>
-        {categories.map((name, index) => (
+        {categories?.map((category, index) => (
           <ListItem key={index} disablePadding>
             <ListItemButton>
-              <Link href={ROUTES.category(name)} className={(pathName == ROUTES.category(name) ? style.active : "")} >{name}</Link>
+              <Link href={ROUTES.category(category.name)} className={(pathName == ROUTES.category(category.name) ? style.active : "")} >{category.name}</Link>
             </ListItemButton>
           </ListItem>
         ))}
