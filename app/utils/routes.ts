4d8599a3583620fd793285fa3ba42c98668c3ea3
valentinaconde <valencondde@gmail.com>
2024-08-21 15:37:39 -0300

    refactor: Add backoffice and admin routes

--- app/utils/routes.ts
+++ app/utils/routes.ts
@@ -2,5 +2,7 @@ export const ROUTES = {
     home: '/home',
     categories: '/home/categories',
     category: (categoryId: number) => `/home/categories/${categoryId}`,
-    recipe: (categoryId: number, recipeId: number) => `/home/categories/${categoryId}/${recipeId}`
+    recipe: (categoryId: number, recipeId: number) => `/home/categories/${categoryId}/${recipeId}`,
+    admin: '/backoffice/admin',
+    login: '/backoffice/login',
 }
