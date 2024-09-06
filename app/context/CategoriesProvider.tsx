f90e71c1fd4f7ace7fe12866809ef3a8496b6c65
valentinaconde <valencondde@gmail.com>
2024-09-06 15:48:37 -0300

    refactor: Update CategoriesProvider to store categories in local storage

--- app/context/CategoriesProvider.tsx
+++ app/context/CategoriesProvider.tsx
@@ -9,12 +9,17 @@ export default function CategoriesProvider({children}: {children: React.ReactNod
     const [categories, setCategories] = useState<Category[]>([])
 
     const handleSetCategories = (categories: Category[]) => {
+        if(localStorage.getItem('categories')) {
+            categories = JSON.parse(localStorage.getItem('categories') || '')
+        }
         setCategories(categories)
     }
-
+   
     const handleAddCategory = (category: Category) => {
         setCategories([...categories, category])
+        localStorage.setItem('categories', JSON.stringify([...categories, category]))
     }
+    
 
     return (
         <CategoriesContext.Provider value={{ categories, handleSetCategories, handleAddCategory }}>{children}</CategoriesContext.Provider>
