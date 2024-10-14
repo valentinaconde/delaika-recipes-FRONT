55a0ba56eeb809698c239e7eda015c09ba0fed7a
valentinaconde <valencondde@gmail.com>
2024-10-14 15:56:12 -0300

    Add page.tsx for managing recipes in backoffice admin

---
+++ app/backoffice/admin/recipes/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import AddRecipe from "./addRecipe.tsx";
+
+export default function Recipes() {
+    return (
+        <>
+            <h1>Administración de recetas</h1>
+            <div>
+                <AddRecipe/>
+
+
+            </div>
+        </>
+
+    )
+}
