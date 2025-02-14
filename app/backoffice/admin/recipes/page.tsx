61f419b94e9a901f883ddb456ee545f1017d543f
Valentina Conde <68123729+valentinaconde@users.noreply.github.com>
2025-02-14 16:23:20 -0300

    Wrap Recipes component with CategoriesProvider for context management

--- app/backoffice/admin/recipes/page.tsx
+++ app/backoffice/admin/recipes/page.tsx
@@ -1,16 +1,17 @@
 'use client'
 import AddRecipe from "./addRecipe.tsx";
+import CateogriesProvider from '../../../context/CategoriesProvider'
 
 export default function Recipes() {
     return (
-        <>
+        <CateogriesProvider>
             <h1>Administración de recetas</h1>
             <div>
                 <AddRecipe/>
 
 
             </div>
-        </>
+        </CateogriesProvider>
 
     )
 }
